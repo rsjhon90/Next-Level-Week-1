b418a9ca78c9805b21bb3461c3aa1ea6a652a639
rsjhon90 <rsjhony@hotmail.com>
2020-06-15 22:37:22 -0300

    Revert "Configuração do servidor"
    This reverts commit ac17b66f14d7db38372cd249d6ce02d952b76d77.

--- src/server.js
+++ src/server.js
@@ -1,19 +1,6 @@
-const express = require("express") // importa library express
+const express = require("express")
 const server = express()
 
-//configurar pasta pública
-server.use(express.static("public"))
-
-//configurar caminhos da aplicação
-//página inicial
-//req: Request/Requisição/Solicitação
-//res: Response/Resposta
-server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
-})
-
-server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
-})
 //ligar o servidor
+
 server.listen(3000)
