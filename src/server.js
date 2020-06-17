b2adeae7adef8e1c35de56980fb6241cc5919bd3
rsjhon90 <rsjhony@hotmail.com>
2020-06-17 18:34:54 -0300

    Template Engine
    Lib nunjucks;
    Renderização;
    Dinamismo do html: variáveis, include

--- src/server.js
+++ src/server.js
@@ -4,16 +4,29 @@ const server = express()
 //configurar pasta pública
 server.use(express.static("public"))
 
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //configurar caminhos da aplicação
 //página inicial
 //req: Request/Requisição/Solicitação
 //res: Response/Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+
 //ligar o servidor
 server.listen(3000)
