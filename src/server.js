4c59094387f2227e5f41dbf64a6c5636d0c7014d
rsjhon90 <rsjhony@hotmail.com>
2020-06-21 21:02:04 -0300

    Consulta do banco de dados
    Confgurando a aplicação para consulta dos dados pelo front-end.

--- src/server.js
+++ src/server.js
@@ -83,9 +83,16 @@ server.post("/savepoint", (req, res) => {
 
 server.get("/search-results", (req, res) => {
 
+    const search = req.query.search
+
+    if(search == "") {
+        // pesquisa vazia
+        return res.render("search-results.html", { total: 0 })  
+    }
+
 
     //pegar os dados do banco de dados
-    db.all(`SELECT * FROM places`, function (err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
         if (err) {
             return console.log(err)
         }
